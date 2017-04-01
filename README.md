b16d379674d5cb0e1e75d3b540a20f78abd58987
clemTheDasher <clemTheDasher@gmail.com>
2017-04-02 06:30:14 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # `<RichTextInput>` for admin-on-rest (with image handler)
 
 For editing HTML with [admin-on-rest](https://github.com/marmelab/admin-on-rest), use the `<RichTextInput>` component. It embarks [quill](http://quilljs.com/), a popular cross-platform Rich Text Editor.
+**This version can use a upload API to upload image to remote storage, and then get a remote url instead of base64 data url**
 
 ![`<RichTextInput>` example](http://marmelab.com/admin-on-rest/img/rich-text-input.png)
 
 ## Installation
 
 ```sh
-npm install aor-rich-text-input --save-dev
+npm install aor-rich-text-input-with-image-handler --save-dev
 ```
 
 ## Usage
@@ -21,18 +22,18 @@ import {
     LongTextInput,
     TextInput,
 } from 'admin-on-rest/mui';
-import RichTextInput from 'aor-rich-text-input';
+import RichTextInput from 'aor-rich-text-input-with-image-handler';
 
 const PostTitle = ({ record }) => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
 };
-
+const uploadResourceAPI = "http://restful.demo/api/v1/" + "resources"
 export const PostEdit = (props) => (
     <Edit title={<PostTitle />} {...props}>
         <DisabledInput label="Id" source="id" />
         <TextInput source="title" validation={{ required: true }} />
         <LongTextInput source="teaser" validation={{ required: true }} />
-        <RichTextInput source="body" validation={{ required: true }} />
+        <RichTextInput source="body" validation={{ required: true }} uploadAPI={uploadResourceAPI} />
         <DateInput label="Publication date" source="published_at" />
     </Edit>
 );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import debounce from 'lodash.debounce';
 import React, { Component, PropTypes } from 'react';
 import Quill from 'quill';
+import Delta from 'quill-delta';
 
 require('./RichTextInput.css');
 
@@ -11,13 +12,72 @@ class RichTextInput extends Component {
                 value,
             },
             toolbar,
+            uploadAPI
         } = this.props;
+        // add a image handler
+        const imageHandler = () => {
+          const that = this.quill.getModule('toolbar')
+
+          let fileInput = that.container.querySelector('input.ql-image[type=file]');
+          if (fileInput == null) {
+            fileInput = document.createElement('input');
+            fileInput.setAttribute('type', 'file');
+            fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon, image/svg+xml');
+            fileInput.classList.add('ql-image');
+
+            //TODO: Modify this handler to upload image to server by REST.
+            fileInput.addEventListener('change', () => {
+              if (fileInput.files != null && fileInput.files[0] != null) {
+                const resource = fileInput.files[0],
+                // 1. store the image and get the image url.
+                  formData = new FormData()
+                formData.append('resource', resource)
+                fetch(uploadAPI, {
+                  method: 'post',
+                  mode: 'cors',
+                  headers: {
+                    "Accept": 'application/json',
+                    "Authorization": 'Bearer ' + localStorage.getItem('token')
+                  },
+                  body: formData
+                })
+                  .then(response => response.json())
+                  .then((result) => {
+                    if (result.resource) {
+                      // 1. Get resource url.
+                      const imageUrl = result.resource.url
+                      // 2. insert the image into the editor.
+                      let range = that.quill.getSelection(true);
+                      that.quill.updateContents(new Delta()
+                        .retain(range.index)
+                        .delete(range.length)
+                        .insert({ image: imageUrl })
+                      , 'user');
+                      fileInput.value = ""
+                    }
+                    else if (result.error) {
+                      fileInput.value = ""
+                      throw result.error
+                    }
+                  }).catch((error) => {
+                    console.log('Request failed', error)
+                  })
+              }
+            })
+            that.container.appendChild(fileInput);
+          }
+          fileInput.click();
+        }
+
+        toolbar.handlers = {
+          "image": imageHandler 
+        } 
 
         this.quill = new Quill(this.divRef, {
             modules: { toolbar },
             theme: 'snow',
         });
-
+        
         this.quill.pasteHTML(value);
 
         this.editor = this.divRef.querySelector('.ql-editor');
@@ -50,6 +110,7 @@ RichTextInput.propTypes = {
     options: PropTypes.object,
     source: PropTypes.string,
     toolbar: PropTypes.oneOfType([
+        PropTypes.object,
         PropTypes.array,
         PropTypes.bool,
     ]),
